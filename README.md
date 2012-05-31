fb0d0ea2a225b21c56950ebfb6253c83648ed9c9
Cameron Johnston <cameron@needle.com>
2012-05-31 12:27:36 -0600

    initial commit, ver 0.0.1

---
+++ README.md
@@ -0,0 +1,14 @@
+Description
+===========
+
+Install the appropriate zeroconf service so that machines can find each other.
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
+
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Cameron Johnston"
+maintainer_email "cameron@needle.com"
+license          "Apache 2.0"
+description      "Installs a zeroconf mDNS daemon"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
