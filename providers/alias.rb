5dfced32f43538cc7e877b50cc99b44de06aa545
Cameron Johnston <cameron@needle.com>
2012-06-05 12:39:44 -0600

    remove @ scoping from new_resource in alias provider

--- providers/alias.rb
+++ providers/alias.rb
@@ -1,25 +1,25 @@
 action :add do
 
-  bash "avahi-publish-alias-#{@new_resource.name}" do
+  bash "avahi-publish-alias-#{new_resource.name}" do
     code <<-EOH
       /usr/bin/avahi-publish-aliases
     EOH
     action :nothing
   end
 
-  file ::File.join('/etc/avahi/aliases.d',@new_resource.name) do
-    content @new_resource.name
+  file ::File.join('/etc/avahi/aliases.d',new_resource.name) do
+    content new_resource.name
     action :create
-    notifies :run, "bash[avahi-publish-alias-#{@new_resource.name}]"
+    notifies :run, "bash[avahi-publish-alias-#{new_resource.name}]"
   end
 
 end
 
 action :remove do
 
-  bash "avahi-remove-alias-#{@new_resource.name}" do
+  bash "avahi-remove-alias-#{new_resource.name}" do
     code <<-EOH
-      /usr/bin/avahi-remove-alias #{@new_resource.name}
+      /usr/bin/avahi-remove-alias #{new_resource.name}
     EOH
   end
 
