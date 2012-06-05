c22d9c7f1f92b31ab2405dfebc8a95fd444287cc
Cameron Johnston <cameron@needle.com>
2012-06-04 19:40:06 -0600

    updating metadata with dependency on ark and bumping version to 0.0.3

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,6 @@ maintainer_email "cameron@needle.com"
 license          "Apache 2.0"
 description      "Installs a zeroconf mDNS daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
+supports         "ubuntu"
+depends          "ark"
