93d6cdf7e8134b59ee5109e6e34e742e757c1733
Cameron Johnston <cameron@needle.com>
2013-06-25 13:54:15 -0600

    making browse domains configurable via attrs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default[:avahi][:hostname] = nil
 default[:avahi][:domain] = "local"
+default[:avahi][:browse_domains] = Array.new
 default[:avahi][:use_ipv4] = true
 default[:avahi][:use_ipv6] = true
 default[:avahi][:allow_interfaces] = Array.new
