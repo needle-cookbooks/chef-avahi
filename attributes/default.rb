ec8821e7791e726c263555e2ff37b83e2a8ed744
Cameron Johnston <cameron@needle.com>
2013-06-25 10:53:22 -0600

    making ipv4 and ipv6 configurable via attrs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
 default[:avahi][:domain] = "local"
+default[:avahi][:use_ipv4] = true
+default[:avahi][:use_ipv6] = true
 default[:avahi][:allow_interfaces] = Array.new
 default[:avahi][:deny_interfaces] = Array.new
